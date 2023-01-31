a8cdd3be0ff73678f0a83430a9314e2000f9cadb
Alexandru997 <besliualexandru33@gmail.com>
2023-01-31 22:28:06 +0200

    [update] style organisation, add theme, save into local storage, create custom hook for theme.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import {Suspense} from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss';
+import './components/styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>HomePage</Link>
             <Link to={'/about'}>AboutUS</Link>
             <Suspense fallback={<div>Loading...</div>}>
