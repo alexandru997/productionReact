2161f5c25ccba872592417b958b1dcf870b9416f
Alexandru997 <besliualexandru33@gmail.com>
2023-01-31 09:08:51 +0200

    [add] webpack dev server, send params for build dev and prod

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -6,8 +6,14 @@ export interface BuildPaths {
     html: string;
 }
 
+export interface BuildEnv {
+    mode: BuildMode;
+    port: number;
+}
+
 export interface BuildOption {
     mode: BuildMode;
     paths: BuildPaths;
     isDev: boolean;
+    port: number;
 }
