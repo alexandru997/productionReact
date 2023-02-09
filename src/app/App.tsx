5806a08d1717ad06e6f0af2c689fe57352cc2d9c
Alexandru997 <besliualexandru33@gmail.com>
2023-02-09 19:09:03 +0200

    [update] add navigation bar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './styles/index.scss';
-import {classNames} from "shared/lib/classNames/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import AppRoute from "./providers/router/ui/AppRoute";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Toggle</button>
-            <Link to={'/'}>HomePage</Link>
-            <Link to={'/about'}>AboutUS</Link>
+            <Navbar/>
             <AppRoute/>
+            <button onClick={toggleTheme}>Toggle</button>
         </div>
     );
 };
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss';
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>HomePage</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>AboutUS</AppLink>
+            </div>
+        </div>
+    );
+};
+
