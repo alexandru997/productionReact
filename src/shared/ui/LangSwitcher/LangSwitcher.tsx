4e1f52114b91b56f8af750c9c7b492ae26b40652
Alexandru997 <besliualexandru33@gmail.com>
2023-03-18 17:56:45 +0200

    [add] language for application, create new component lang switcher, add plugin for i18next define plugin

---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,25 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTranslation} from 'react-i18next';
+import cls from './LangSwitcher.module.scss'
+import i18n from "i18next";
+import {Button, ThemeButton} from "shared/ui/Button/Button";
+
+interface LangSwitcherProps {
+    className?: string
+}
+
+export const LangSwitcher = ({className}: LangSwitcherProps) => {
+
+    const {t} = useTranslation();
+
+    const toggle = async () => {
+        i18n.changeLanguage(i18n.language === 'ro' ? 'en' : 'ro')
+    }
+
+    return (
+        <Button
+            className={classNames(cls.LangSwitcher, {}, [className])}
+            theme={ThemeButton.CLEAR} onClick={toggle}>{t('Language')}</Button>
+    );
+};
+
