a6fad40ebe0a0575ff438f9dc3d7d8e6c2eb0211
Alexandru997 <besliualexandru33@gmail.com>
2023-03-27 14:49:54 +0300

    [add] ErrorBoundary, Handling React Errors

---
+++ src/app/providers/ErrorBoundary/index.ts
@@ -0,0 +1,7 @@
+import ErrorBoundary from './ui/ErrorBoundary';
+import { ErrorButtonBug } from './ui/ErrorButtonBug';
+
+export {
+    ErrorBoundary,
+    ErrorButtonBug,
+};
---
+++ src/app/providers/ErrorBoundary/ui/ErrorButtonBug.tsx
@@ -0,0 +1,22 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import { useEffect, useState } from 'react';
+
+// It's component for testings ErrorBoundary
+export const ErrorButtonBug = () => {
+    const [error, setError] = useState(false);
+    const { t } = useTranslation();
+    const throwClick = () => setError(true);
+
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+    return (
+        <Button onClick={throwClick}>
+            {t('trigege error')}
+        </Button>
+    );
+};
