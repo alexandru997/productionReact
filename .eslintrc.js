8846b3adf3eade4012a088dfe19188d754858be3
Alexandru997 <besliualexandru33@gmail.com>
2023-03-28 11:59:31 +0300

    [add] Testing library, and test button and sidebar component, add configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,10 +39,22 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': ['error', {
+            markupOnly: true,
+            ignoreAttribute: ['data-testid', 'to'],
+        }],
         'max-len': ['error', { ignoreComments: true, code: 140 }],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+                'max-len': 'off',
+            },
+        },
+    ],
 };
