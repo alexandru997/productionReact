d3b365c4df35ae3b3bbfa8eb70b93930062568e7
Alexandru997 <besliualexandru33@gmail.com>
2023-05-13 14:18:54 +0300

    [add] Fix errors on project after typeScript strict mode

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -5,14 +5,14 @@ import { useDispatch } from 'react-redux';
 import { $api } from 'shared/api/api';
 import { To } from '@remix-run/router';
 import { NavigateOptions } from 'react-router/dist/lib/context';
+import { CombinedState, Reducer } from 'redux';
 import { createReducerManager } from './reducerManager';
-import { StateSchema } from './StateSchema';
+import { StateSchema, ThunkExtraArg } from './StateSchema';
 
 export function createReduxStore(
     initialState?: StateSchema,
     asyncReducers?: ReducersMapObject<StateSchema>,
     navigate?: (to: To, options?: NavigateOptions) => void,
-
 ) {
     const rootReducers: ReducersMapObject<StateSchema> = {
         ...asyncReducers,
@@ -23,17 +23,18 @@ export function createReduxStore(
 
     const reducerManager = createReducerManager(rootReducers);
 
+    const extraArg: ThunkExtraArg = {
+        api: $api,
+        navigate,
+    };
+
     const store = configureStore({
-        reducer: reducerManager.reduce,
+        reducer: reducerManager.reduce as Reducer<CombinedState<StateSchema>>,
         devTools: __IS_DEV__,
         preloadedState: initialState,
         middleware: (getDefaultMiddleware) => getDefaultMiddleware({
             thunk: {
-                extraArgument: {
-                    api: $api,
-                    navigate,
-
-                },
+                extraArgument: extraArg,
             },
         }),
     });
--- src/pages/Profile/ui/ProfilePage.tsx
+++ src/pages/Profile/ui/ProfilePage.tsx
@@ -2,6 +2,9 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { profileReducer } from 'entities/Profile/model/slice/profileSlice';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useEffect } from 'react';
+import { fetchProfileData, ProfileCard } from 'entities/Profile';
 
 const reducers: ReducersList = {
     profile: profileReducer,
@@ -9,11 +12,16 @@ const reducers: ReducersList = {
 
 const ProfilePage = () => {
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames('', {}, [])}>
-                {t('PROFILE_PAGE')}
+                <ProfileCard />
 
             </div>
         </DynamicModuleLoader>
