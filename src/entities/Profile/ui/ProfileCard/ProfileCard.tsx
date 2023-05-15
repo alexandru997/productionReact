9d5e52fdfcc34d81d5a7070d6bc202ec6fd08973
ABesliu <alexandru.besliu@orange.com>
2023-05-15 12:28:05 +0300

    [add] second commit for Module Profile

--- src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -1,9 +1,12 @@
-import { classNames } from 'shared/lib/classNames/classNames';
+import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Text, TextAlign, TextTheme } from 'shared/ui/Text/Text';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
 import { Loader } from 'shared/ui/Loader/Loader';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Select } from 'shared/ui/Select/Select';
+import { Currency } from 'shared/const/common';
 import { Profile } from '../../model/types/profile';
 import cls from './ProfileCard.module.scss';
 
@@ -57,11 +60,18 @@ export const ProfileCard = (props: ProfileCardProps) => {
         );
     }
 
-    return (
-        <div className={classNames(cls.ProfileCard, {}, [className])}>
-            <div className={cls.data}>
+    const mods:Mods = {
+        [cls.editing]: !readonly,
+    };
 
-                {data?.avatar && <img src={data?.avatar} alt="" />}
+    return (
+        <div className={classNames(cls.ProfileCard, mods, [className])}>
+            <div className={cls.data}>
+                {data?.avatar && (
+                    <div className={cls.avatarWrapper}>
+                        <Avatar src={data?.avatar} alt="" />
+                    </div>
+                )}
                 <Input
                     value={data?.first}
                     placeholder={t('Last name')}
@@ -108,6 +118,16 @@ export const ProfileCard = (props: ProfileCardProps) => {
                     onChange={onChangeAvatar}
                     readonly={readonly}
 
+                />
+                <Select
+                    label={t('Select currency')}
+                    options={
+                        [
+                            { value: Currency.MDL, content: Currency.MDL },
+                            { value: Currency.EUR, content: Currency.EUR },
+                            { value: Currency.USD, content: Currency.USD },
+                        ]
+                    }
                 />
             </div>
         </div>
