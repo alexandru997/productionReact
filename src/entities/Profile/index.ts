25576c495b3dcadb2696120bdec9400d500fe0cf
ABesliu <alexandru.besliu@orange.com>
2023-05-15 10:37:58 +0300

    [add] first commit for Module Profile

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -11,7 +11,16 @@ export {
 export {
     fetchProfileData,
 } from './model/services/fetchProfileData/fetchProfileData';
+export {
+    updateProfileData,
+} from './model/services/updateProfileData/updateProfileData';
 
 export {
     ProfileCard,
 } from './ui/ProfileCard/ProfileCard';
+
+export { getProfileData } from './model/selectors/getProfileData/getProfileData';
+export { getProfileError } from './model/selectors/getProfileError/getProfileError';
+export { getProfileIsLoading } from './model/selectors/getProfileIsLoading/getProfileIsLoading';
+export { getProfileReadOnly } from './model/selectors/getProfileReadOnly/getProfileReadOnly';
+export { getProfileForm } from './model/selectors/getProfileForm/getProfileForm';
--- src/pages/Profile/ui/ProfilePage.tsx
+++ src/pages/Profile/ui/ProfilePage.tsx
@@ -1,10 +1,20 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { profileReducer } from 'entities/Profile/model/slice/profileSlice';
+import { profileActions, profileReducer } from 'entities/Profile/model/slice/profileSlice';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { useEffect } from 'react';
-import { fetchProfileData, ProfileCard } from 'entities/Profile';
+import { useCallback, useEffect } from 'react';
+import {
+    fetchProfileData,
+    getProfileData,
+    getProfileError,
+    getProfileForm,
+    getProfileIsLoading,
+    getProfileReadOnly,
+    ProfileCard,
+} from 'entities/Profile';
+import { useSelector } from 'react-redux';
+import { ProfilePageHeader } from './ProfilePageHeader/ProfilePageHeader';
 
 const reducers: ReducersList = {
     profile: profileReducer,
@@ -13,16 +23,54 @@ const reducers: ReducersList = {
 const ProfilePage = () => {
     const { t } = useTranslation();
     const dispatch = useAppDispatch();
-
+    const formData = useSelector(getProfileForm);
+    const isLoading = useSelector(getProfileIsLoading);
+    const error = useSelector(getProfileError);
+    const readonly = useSelector(getProfileReadOnly);
     useEffect(() => {
         dispatch(fetchProfileData());
+    }, [dispatch]);
+
+    const onChangeLastname = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ lastname: value || '' }));
+    }, [dispatch]);
+    const onChangeFirstname = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ first: value || '' }));
+    }, [dispatch]);
+
+    const onChangeCity = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ city: value || '' }));
+    }, [dispatch]);
+
+    const onChangeAge = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ age: Number(value || 0) }));
+    }, [dispatch]);
+
+    const onChangeUsername = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ username: value || '' }));
+    }, [dispatch]);
+
+    const onChangeAvatar = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ avatar: value || '' }));
     }, [dispatch]);
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames('', {}, [])}>
-                <ProfileCard />
+                <ProfilePageHeader />
+                <ProfileCard
+                    data={formData}
+                    isLoading={isLoading}
+                    error={error}
+                    readonly={readonly}
+                    onChangeLastname={onChangeFirstname}
+                    onChangeFirstname={onChangeLastname}
+                    onChangeCity={onChangeCity}
+                    onChangeAge={onChangeAge}
+                    onChangeUsername={onChangeUsername}
+                    onChangeAvatar={onChangeAvatar}
 
+                />
             </div>
         </DynamicModuleLoader>
     );
