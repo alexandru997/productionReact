3ce08ac93d8652f48298490e8649d454e050fc47
ABesliu <alexandru.besliu@orange.com>
2023-05-17 18:47:45 +0300

    [add] react refresh plugin

---
+++ config/build/loaders/buildBabelLoader.ts
@@ -0,0 +1,24 @@
+import { BuildOption } from '../types/config';
+
+export function buildBabelLoader({ isDev }:BuildOption) {
+    return {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        {
+                            locales: ['en', 'ro'],
+                            keyAsDefaultValue: true,
+                        },
+                    ],
+                    isDev && require.resolve('react-refresh/babel'),
+                ].filter(Boolean),
+            },
+        },
+    };
+}
